eaa28625fe1ac2ec27e2637c600484140081749d
koketsolk <koketsolekau86@gmail.com>
2023-05-18 09:26:54 -0700

    size of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (usigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (usigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (usigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (usigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (usigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (usigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (usigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (usigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (usigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (usigned long)sizeof(f));
 	return (0);
 }
